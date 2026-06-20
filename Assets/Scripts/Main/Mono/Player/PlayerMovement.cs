e01c47f0a790e64df0081b387aa3fee84219e21f
= <26445308+GingaaBread@users.noreply.github.com>
2026-06-21 01:42:22 +0200

    feat(Animation): prototype animation

--- Assets/Scripts/Main/Mono/Player/PlayerMovement.cs
+++ Assets/Scripts/Main/Mono/Player/PlayerMovement.cs
@@ -1,25 +1,62 @@
+using Main.Service;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using VContainer;
 
 namespace Main.Mono.Player
 {
     [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerMovement : MonoBehaviour
     {
+        private static readonly int IS_MOVING_ID = Animator.StringToHash("Is Walking");
+        private static readonly int IS_DRILLING_ID = Animator.StringToHash("Is Drilling");
         [SerializeField] private float _defaultMovementSpeed = 5f;
         [SerializeField] private bool _isAllowedToMoveUp;
-        private Vector2 _currentMovementData;
+        [SerializeField] private Animator _animator;
 
+        [Inject] private readonly DrillActivationMediator _drillActivationMediator;
+        private Vector2 _currentMovementData;
         private Rigidbody2D _rigidbody2D;
+
+        private bool _wasMoving;
 
         private void Awake()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
+        private void Update()
+        {
+            var isIdle = Mathf.Approximately(0f, _rigidbody2D.linearVelocity.x) &&
+                         Mathf.Approximately(0f, _rigidbody2D.linearVelocity.y);
+            var isMoving = !isIdle;
+
+            if (isMoving == _wasMoving) return;
+
+            _animator.SetBool(IS_MOVING_ID, isMoving);
+            _wasMoving = isMoving;
+        }
+
         private void FixedUpdate()
         {
             _rigidbody2D.linearVelocity = _currentMovementData * _defaultMovementSpeed;
+        }
+
+        private void OnEnable()
+        {
+            if (_drillActivationMediator == null) return;
+            _drillActivationMediator.OnActivationChange += UpdateDrillAnimator;
+        }
+
+        private void OnDisable()
+        {
+            if (_drillActivationMediator == null) return;
+            _drillActivationMediator.OnActivationChange -= UpdateDrillAnimator;
+        }
+
+        private void UpdateDrillAnimator(bool isDrilling)
+        {
+            _animator.SetBool(IS_DRILLING_ID, isDrilling);
         }
 
         public void TryMove(InputAction.CallbackContext ctx)
