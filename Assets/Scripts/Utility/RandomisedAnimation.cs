414acbd79e61ed03684f5a0e2ee28f5f6e87b968
= <26445308+GingaaBread@users.noreply.github.com>
2026-06-26 18:00:32 +0200

    chore(Asset Replacements)

---
+++ Assets/Scripts/Utility/RandomisedAnimation.cs
@@ -0,0 +1,25 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Utility
+{
+    [RequireComponent(typeof(Animator))]
+    public class RandomisedAnimation : MonoBehaviour
+    {
+        [Required] [SerializeField] private Animator _animator;
+        [SerializeField] private string _stateName;
+
+        private void Awake()
+        {
+            PlayRandomised();
+        }
+
+        private void PlayRandomised()
+        {
+            var randomTime = Random.value; // 0.0 - 1.0
+            _animator.Play(_stateName, 0, randomTime);
+            _animator.Update(0f); // Immediately apply the new state
+        }
+    }
+}
