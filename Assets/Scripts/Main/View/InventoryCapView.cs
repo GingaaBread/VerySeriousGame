49a8a30754da19b11e76e6303e6371e28d85f695
= <26445308+GingaaBread@users.noreply.github.com>
2026-06-27 01:27:33 +0200

    feat(Inventory): inventory size view

---
+++ Assets/Scripts/Main/View/InventoryCapView.cs
@@ -0,0 +1,31 @@
+﻿using Main.Service;
+using TMPro;
+using UnityEngine;
+using VContainer;
+
+namespace Main.View
+{
+    public class InventoryCapView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _amountText;
+
+        [Inject] private readonly PlayerInventoryService _playerInventoryService;
+
+        private void OnEnable()
+        {
+            _playerInventoryService.OnInventoryCapUpdated += Render;
+            var cap = _playerInventoryService.GetCurrentCap();
+            Render(cap.Item1, cap.Item2);
+        }
+
+        private void OnDisable()
+        {
+            _playerInventoryService.OnInventoryCapUpdated += Render;
+        }
+
+        private void Render(int heldAmount, int maxAmount)
+        {
+            _amountText.text = heldAmount + "/" + maxAmount;
+        }
+    }
+}
